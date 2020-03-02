2e3929dbf6b057042bb8a8de086077edc9dd1acc
Ryuu11 <60558023+Ryuu11@users.noreply.github.com>
2020-03-01 21:35:20 -0400

    TareaProductos

---
+++ Bruh/Controllers/PROVEEDORsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bruh.Models;
+
+namespace Bruh.Controllers
+{
+    public class PROVEEDORsController : Controller
+    {
+        private ProductsEntities db = new ProductsEntities();
+
+        // GET: PROVEEDORs
+        public ActionResult Index()
+        {
+            return View(db.PROVEEDORs.ToList());
+        }
+
+        // GET: PROVEEDORs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PROVEEDOR pROVEEDOR = db.PROVEEDORs.Find(id);
+            if (pROVEEDOR == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pROVEEDOR);
+        }
+
+        // GET: PROVEEDORs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PROVEEDORs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID_PROV,NOMBRE,DIRECCION,NUM_TELF")] PROVEEDOR pROVEEDOR)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PROVEEDORs.Add(pROVEEDOR);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pROVEEDOR);
+        }
+
+        // GET: PROVEEDORs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PROVEEDOR pROVEEDOR = db.PROVEEDORs.Find(id);
+            if (pROVEEDOR == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pROVEEDOR);
+        }
+
+        // POST: PROVEEDORs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID_PROV,NOMBRE,DIRECCION,NUM_TELF")] PROVEEDOR pROVEEDOR)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pROVEEDOR).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pROVEEDOR);
+        }
+
+        // GET: PROVEEDORs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PROVEEDOR pROVEEDOR = db.PROVEEDORs.Find(id);
+            if (pROVEEDOR == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pROVEEDOR);
+        }
+
+        // POST: PROVEEDORs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PROVEEDOR pROVEEDOR = db.PROVEEDORs.Find(id);
+            db.PROVEEDORs.Remove(pROVEEDOR);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Bruh/Controllers/ProductoesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bruh.Models;
+
+namespace Bruh.Controllers
+{
+    public class ProductoesController : Controller
+    {
+        private ProductsEntities db = new ProductsEntities();
+
+        // GET: Productoes
+        public ActionResult Index()
+        {
+            var productoes = db.Productoes.Include(p => p.PROVEEDOR);
+            return View(productoes.ToList());
+        }
+
+        // GET: Productoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productoes.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: Productoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID_PROVEEDOR = new SelectList(db.PROVEEDORs, "ID_PROV", "NOMBRE");
+            return View();
+        }
+
+        // POST: Productoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,NOMBRE,DESCRIPCION,FECHA_VENC,ID_PROVEEDOR")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productoes.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_PROVEEDOR = new SelectList(db.PROVEEDORs, "ID_PROV", "NOMBRE", producto.ID_PROVEEDOR);
+            return View(producto);
+        }
+
+        // GET: Productoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productoes.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID_PROVEEDOR = new SelectList(db.PROVEEDORs, "ID_PROV", "NOMBRE", producto.ID_PROVEEDOR);
+            return View(producto);
+        }
+
+        // POST: Productoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NOMBRE,DESCRIPCION,FECHA_VENC,ID_PROVEEDOR")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID_PROVEEDOR = new SelectList(db.PROVEEDORs, "ID_PROV", "NOMBRE", producto.ID_PROVEEDOR);
+            return View(producto);
+        }
+
+        // GET: Productoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productoes.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Productoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Producto producto = db.Productoes.Find(id);
+            db.Productoes.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
